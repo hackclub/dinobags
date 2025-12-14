d679e3a0d15d0663108e6e6e2897a77e21ed0eb0
transcental <amber@transcental.dev>
2025-12-14 05:52:56 +0000

    remove sender check

--- app/mailboxes/hcb_mailbox.rb
+++ app/mailboxes/hcb_mailbox.rb
@@ -7,8 +7,6 @@ class HCBMailbox < ApplicationMailbox
   def process
     Rails.logger.info("Processing #{mail.subject} from #{mail.sender}")
 
-    return unless mail.sender == "hcb@hackclub.com"
-
     handler = HANDLERS.find { |h| h.matches?(mail) }
     handler&.new(mail)&.call
   end
