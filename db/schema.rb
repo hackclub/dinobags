020a5a189d12aaff7f5535368749538025b54b06
transcental <amber@transcental.dev>
2025-12-14 15:27:16 +0000

    move to single db setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_14_132955) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_14_152551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -59,6 +59,11 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_14_132955) do
     t.datetime "created_at", null: false
     t.string "redirect_uri"
     t.string "refresh_token_ciphertext"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "solid_tables", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
