db7380fe1975c4be2560e9d985e42a2c7abd7cca
transcental <amber@transcental.dev>
2025-12-15 03:26:28 +0000

    remove sentry test

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,16 +3,7 @@ class LandingController < ApplicationController
   skip_after_action :verify_authorized
 
   def index
-    # redirect_to dashboard_path if user_signed_in?
-
-    begin
-      1 / 0
-    rescue ZeroDivisionError => exception
-      Rails.logger.info "waa"
-      Sentry.capture_exception(exception)
-    end
-
-    Sentry.capture_message("test message")
+    redirect_to dashboard_path if user_signed_in?
 
     @total_transferred = Topup.sum(:amount_cents)
     @total_txns = Topup.count
